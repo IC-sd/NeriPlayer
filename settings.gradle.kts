7a4cc46322b85e7cfadf3bdf0782b111315a583f
IC-sd <1228528769sky@gmail.com>
2026-07-06 09:40:43 +0800

    fix: 替换 jvmToolchain 为标准编译配置，修复 JDK 依赖问题

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,10 +11,9 @@ pluginManagement {
         maven { url = uri("https://jitpack.io") }
         gradlePluginPortal()
     }
-}
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.9.0")
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version("0.9.0")
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
